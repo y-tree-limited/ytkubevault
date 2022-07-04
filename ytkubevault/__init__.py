1cfd1be1944618929347eadd9f3cc6a0038682c1
Wenru Dong <wenru@y-tree.com>
2022-07-05 04:49:39 +0800

    bump version

--- ytkubevault/__init__.py
+++ ytkubevault/__init__.py
@@ -1,13 +1,21 @@
-__version__ = "0.0.1"
+__version__ = "0.1.0"
 import logging
 from logging import NullHandler
 
-from .vault import get_secret_or_env, get_vault_secret_keys, is_vault_initialised
+from .vault import (
+    encrypt_or_default,
+    decrypt_or_default,
+    get_secret_or_env,
+    get_vault_secret_keys,
+    is_vault_initialised
+)
 
 __all__ = [
     "get_secret_or_env",
     "get_vault_secret_keys",
-    "is_vault_initialised"
+    "is_vault_initialised",
+    "encrypt_or_default",
+    "decrypt_or_default"
 ]
 
 logging.getLogger(__name__).addHandler(NullHandler())
